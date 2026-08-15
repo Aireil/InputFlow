83647d72a65b5d66ace67c4879b6a66b435580ab
Aireil <33433913+Aireil@users.noreply.github.com>
2026-08-15 22:38:45 +0200

    Clean sample-related code

---
+++ InputFlow/Plugin.cs
@@ -0,0 +1,57 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using InputFlow.Windows;
+
+namespace InputFlow;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+
+    private const string CommandName = "/inputflow";
+
+    public Configuration Configuration { get; init; }
+
+    public readonly WindowSystem WindowSystem = new("InputFlow");
+    private ConfigWindow ConfigWindow { get; init; }
+
+    public Plugin()
+    {
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+        ConfigWindow = new ConfigWindow(this);
+
+        WindowSystem.AddWindow(ConfigWindow);
+
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Open config"
+        });
+
+        PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
+    }
+
+    public void Dispose()
+    {
+        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
+        
+        WindowSystem.RemoveAllWindows();
+
+        ConfigWindow.Dispose();
+
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        ToggleConfigUi();
+    }
+
+    public void ToggleConfigUi() => ConfigWindow.Toggle();
+}
---
+++ InputFlow/Windows/ConfigWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Windowing;
+
+namespace InputFlow.Windows;
+
+public class ConfigWindow : Window, IDisposable
+{
+    private readonly Configuration configuration;
+
+    public ConfigWindow(Plugin plugin) : base("InputFlow Config")
+    {
+        Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
+                ImGuiWindowFlags.NoScrollWithMouse;
+
+        Size = new Vector2(232, 90);
+        SizeCondition = ImGuiCond.Always;
+
+        configuration = plugin.Configuration;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        var movable = configuration.IsConfigWindowMovable;
+        if (ImGui.Checkbox("Movable Config Window", ref movable))
+        {
+            configuration.IsConfigWindowMovable = movable;
+            configuration.Save();
+        }
+    }
+}
